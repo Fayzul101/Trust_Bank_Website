9b6cc88505dc17b95c39ea6044c0c5279ed6d7ef
Fayzul Noor Tabeeb <fayzul.noor@gmail.com>
2025-02-02 12:07:02 +0600

    initial_commit

---
+++ script.js
@@ -0,0 +1,379 @@
+let products = [
+      {
+        id: "modal-1",
+        productName: "Arong",
+        category: "Brand_Shop",
+        image: "Brand_images/490px-Aarong_logo_0.jpg",
+        description: '<span style="font-weight:600">Discount / Offer on total invoice in all outlets of Aarong around the country:</span><br><ul><li>TBL Platinum Credit Card Holders are eligible for 5% discount .</li><li>TBL Signature Credit Card Holders are eligible for 10% discount .</li></ul>',
+      },
+      {
+        id: "modal-2",
+        productName: "Fit Elegance",
+        category: "Brand_Shop",
+        image: "Brand_images/Fit Elegance.jpg",
+        description: 'TBL Debit & Credit Cardholders and TBL Employees will enjoy <span style="font-weight:600">10% discount</span> on total bill from <span style="font-weight:600">‘Fit Elegance’</span>.'
+      },
+      {
+        id: "modal-3",
+        productName: "JK Foreign Brand",
+        category: "Brand_shop",
+        price: "99",
+        image: "Brand_images/JK Foreign Brand.jpg",
+        description: "<p>TBL Debit and Credit Cardholders will enjoy <strong>10% Discount</strong> on total bill.<br>JK Foreign Brand is a premium fashion brand focused on bridal and groom collections, having their outlet at <strong>Gulshan</strong>, Dhaka<br><br><strong>Contact Information</strong><br>Address: Plot#76/B, Ground Floor, Khawaja palace, Road 11, Banani, Dhaka, 1213.<br>Mobile: 01711-345801  /  01795-743310<br>Website: https://jkforeignbrands.com</p>"
+      },
+      {
+        id: "modal-4",
+        productName: "Leatherex Fashion & Footwear",
+        category: "Brand_Shop",
+        price: "29",
+        image: "Brand_images/LEATHEREX.png",
+        description: "<p>TBL Debit & Credit Cardholders and TBL Employees will enjoy <strong>10% discount</strong> on total bill.<br><strong>Leatherex</strong> is a well-known company for selling international brand leather shoes and bags. Currently, they have five outlets in Dhaka.<br><strong>Contact Information:</strong><br>Leatherex Fashion and Footwear<br>Contact No – 01819-453766<br>Website: https://leatherex-fashion.com/<br><strong>Outlet Address:</strong><br>1. Banani-Delta Dahlia Tower, 36 Kamal Ataturk Avenue, Dhaka-1213.<br>2. Orchard Point – H#14, R#7 Dhanmondi, Dhaka-1209<br>3. 86, Kazi Nazrul Islam Avenue, Dhaka-1215<br>4. 143/2, New Baily Road, Dhaka-1216<br>5. 56, Garib-E-Nawaz Avenue, Sector -13, Uttara, Dhaka – 1230</p>"
+      },
+      {
+        id: "modal-5",
+        productName: "Royal Diamond",
+        category: "Brand_Shop",
+        price: "129",
+        image: "Brand_images/Royal Diamond.jpg",
+        description: "<p><ul><li><strong>27% discount on total bill</strong> on all the products</li><li>0% EMI ‘Easy Pay’ facility up to 36 months</li></ul><strong>‘Royal Diamond’</strong> is one of the best premium diamond jewelry shops in Bangladesh. At present, they have two showrooms in Dhaka city. They offer attractive prices, ornament exchange & custom design to the customers.<br><strong>Showrooms Address:</strong><br><ul><li>New Royal Diamond, Navana Tower, shop: 11-12-13, (1st Floor), Gulshan-1, Dhaka.<br>Cell: 01772144475.</li><li>Royal Diamond, 2nd floor, Shop: 367, Police Plaza Concord-Gulshan-1, Dhaka.<br>Cell: 01795096980</li></ul></p>"
+      },
+      {
+        id: "modal-6",
+        productName: "Time Access",
+        category: "Brand_Shop",
+        price: "89",
+        image: "Brand_images/Time Access.png",
+      },
+      {
+        id: "modal-7",
+        productName: "Sea Pearl Beach Resort & Spa Cox's Bazar",
+        category: "Hotel,_Resorts_&_Restaurants",
+        price: "189",
+        image: "Brand_images/Sea pearl_0 (1).png",
+      },
+      {
+        id: "modal-8",
+        productName: "Radison Blu Dhaka Water Garden",
+        category: ["Hotel,_Resorts_&_Restaurants", "BOGO_Facility"],
+        price: "189",
+        image: "Brand_images/radisson-1-logo-png-transparent.png",
+        description: 'For TBL Signature Credit Card holders<br> <span style="font-weight:600">Buy One Get One offer</span> during:<br>Dinner <span style="font-weight:600">Other discounts</span> 15% on buffet (breakfast, lunch, dinner).<br>15% on food only at spice and rice restaurants.<br><span style="font-weight:600">**max. 8 persons per booking to get the discount.</span><br><span style="font-weight:600">**No discount on alcohol and beverages.</span>',
+      },
+      {
+        id: "modal-9",
+        productName: "Palm View Restaurant",
+        category: "Hotel,_Resorts_&_Restaurants",
+        price: "189",
+        image: "brand_images/palm view resturant.png",
+      },
+      {
+        id: "modal-10",
+        productName: "InterContinental Dhaka",
+        category: ["Hotel,_Resorts_&_Restaurants", "BOGO_Facility"],
+        price: "189",
+        image: "Brand_images/Intercontinental dhaka.jpg",
+      },
+      {
+        id: "modal-11",
+        productName: "Long Beach Hotel, Cox's Bazar",
+        category: ["Hotel,_Resorts_&_Restaurants", "BOGO_Facility"],
+        price: "189",
+        image: "Brand_images/long beach hotel cox.png",
+      },
+      {
+        id: "modal-12",
+        productName: "MK Electronics",
+        category: ["Electronics_&_Home_Appliances"],
+        price: "189",
+        image: "Brand_images/MK Electronics_0.jpg",
+      },
+      {
+        id: "modal-13",
+        productName: "Best Electronics Ltd",
+        category: ["Electronics_&_Home_Appliances"],
+        price: "189",
+        image: "Brand_images/best_0.jpeg",
+      },
+      {
+        id: "modal-14",
+        productName: "Electra International - Bangladesh",
+        category: ["Electronics_&_Home_Appliances"],
+        price: "189",
+        image: "Brand_images/3.png",
+      },
+      {
+        id: "modal-15",
+        productName: "Pickaboo.com",
+        category: ["Online_Shop"],
+        price: "189",
+        image: "Brand_images/capture.jpeg",
+      },
+      {
+        id: "modal-16",
+        productName: "Care Box",
+        category: ["Online_Shop"],
+        price: "189",
+        image: "Brand_images/Care Box.png",
+      },
+      {
+        id: "modal-17",
+        productName: "GoZayaan",
+        category: ["Online_Shop", "Travel"],
+        price: "189",
+        image: "Brand_images/gozayaan_0.jpeg",
+      },
+      {
+        id: "modal-18",
+        productName: "Travel Factory",
+        category: ["Travel"],
+        price: "189",
+        image: "Brand_images/1718003790190.jpg",
+      },
+      {
+        id: "modal-19",
+        productName: "The Way Dhaka",
+        category: ["Travel"],
+        price: "189",
+        image: "Brand_images/THE WAY DHAKA.jpeg",
+      },
+      {
+        id: "modal-20",
+        productName: "Apple Gadget BD",
+        category: ["Gadget_&_Accessories" , "Mobile_Shop"],
+        price: "189",
+        image: "Brand_images/apple_gadgets.jpg",
+      },
+      {
+        id: "modal-21",
+        productName: "Gadget & Gear",
+        category: ["Gadget_&_Accessories", "Mobile_Shop"],
+        price: "189",
+        image: "Brand_images/gng.png",
+      },
+      {
+        id: "modal-22",
+        productName: "Star Tech & Engineering",
+        category: ["Gadget_&_Accessories","Mobile_Shop"],
+        price: "189",
+        image: "Brand_images/star-tech.webp",
+      },
+      {
+        id: "modal-23",
+        productName: "Transcom Mobile Ltd",
+        category: ["Mobile_Shop"],
+        price: "189",
+        image: "Brand_images/Transcomjpg.jpg",
+      },
+      {
+        id: "modal-24",
+        productName: "Farazy Hospital Ltd",
+        category: ["Hospital_&_Medical_Services"],
+        price: "189",
+        image: "Brand_images/Farazy-Hospital-Ltd.jpg",
+      },
+      {
+        id: "modal-25",
+        productName: "BRB Hospital Ltd",
+        category: ["Hospital_&_Medical_Services"],
+        price: "189",
+        image: "Brand_images/BRB-HOSPITAL.jpg",
+      },
+      {
+        id: "modal-26",
+        productName: "Brothers Furniture Ltd",
+        category: ["Furniture"],
+        price: "189",
+        image: "Brand_images/brother furniture logo_0.png",
+      },
+      {
+        id: "modal-27",
+        productName: "Partex Furniture Industries Ltd",
+        category: ["Furniture"],
+        price: "189",
+        image: "Brand_images/Partex Furniture Industries Ltd.png",
+      },
+      {
+        id: "modal-28",
+        productName: "The HI TECH Lifestyle",
+        category: ["Furniture"],
+        price: "189",
+        image: "Brand_images/The HI TECH Lifestyle.gif",
+      },
+      {
+        id: "modal-29",
+        productName: "Zantrik Ltd",
+        category: ["Automobile"],
+        price: "189",
+        image: "Brand_images/zantrik_0.png",
+      },
+      {
+        id: "modal-30",
+        productName: "Vroom Service Ltd",
+        category: ["Automobile"],
+        price: "189",
+        image: "Brand_images/Vroom Service Limited.png",
+      },
+      {
+        id: "modal-31",
+        productName: "Runner Automobiles Ltd",
+        category: ["Automobile"],
+        price: "189",
+        image: "Brand_images/Runner Automobiles Ltd.png",
+      },
+      {
+        id: "modal-32",
+        productName: "Paystation",
+        category: ["Software_Development_Platform"],
+        price: "189",
+        image: "Brand_images/Service Hub Limited (Paystation).png",
+      },
+      {
+        id: "modal-33",
+        productName: "SSL Commerze",
+        category: ["Software_Development_Platform"],
+        price: "189",
+        image: "Brand_images/SSL Commerce.png",
+      },
+      {
+        id: "modal-34",
+        productName: "Soft Tech Innovation Ltd",
+        category: ["Software_Development_Platform"],
+        price: "189",
+        image: "Brand_images/Soft tech innovation.png",
+      },
+    ];
+
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', // Replace with your MySQL username
+    password: '', // Replace with your MySQL password
+    database: 'test_db', // Replace with your database name
+});
+
+// Connect to the database
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the database as ID', connection.threadId);
+
+    // Fetch data from the database
+    connection.query('SELECT * FROM users', (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err.stack);
+        } else {
+            console.log('Users:', results);
+        }
+        connection.end(); // Close the connection
+    });
+});
+
+  console.log(results[0]);
+
+  const container = document.querySelector(".row");
+
+  products.forEach((member) => {
+    const categoryClasses = Array.isArray(member.category) 
+    ? member.category.map((cat) => ` ${cat}`).join("") 
+    : ` ${member.category}`;
+    container.innerHTML += `
+      <div class="cards ${categoryClasses} col-lg-4 col-md-4 col-sm-6">
+            <a href="#" data-bs-toggle="modal" data-bs-target="#${member.id}" style="text-decoration: none;">
+              <div class="team-member">
+                  <div class="member-card">
+                      <img src="${member.image}" class="member-image img-fluid" style="margin: 10px 0; width: 300px">
+                      <div class="product-name">${member.productName}</div>
+                  </div>
+              </div>
+            </a>
+          </div>
+      <div class="modal fade" id="${member.id}">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5">${member.productName}</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body py-3">
+              <div class="box" style="display: flex;">
+                <div class="image my-auto me-4">
+                  <img src="${member.image}" class="img-fluid" alt="...">
+                </div>
+                <div class="writting" style="">
+                  <div class="card-body" style="width: 510px;">
+                    ${member.description}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+  
+  //parameter passed from button (Parameter same as category)
+  function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      value1 = value.replace(/_/g, " ");
+      console.log(value1);
+      if (value1.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  
+    //select all cards
+    let elements = document.querySelectorAll(".cards");
+    //loop through all cards
+    elements.forEach((element) => {
+      //display all cards on 'all' button click
+      if (value == "all") {
+        element.classList.remove("hide");
+      } else {
+        //Check if element contains category class
+        if (element.classList.contains(value)) {
+          //display element based on category
+          element.classList.remove("hide");
+        } else {
+          //hide other elements
+          element.classList.add("hide");
+        }
+      }
+    });
+  }
+  
+  //Search button click
+  document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".cards");
+  
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.toUpperCase().includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+  
+  //Initially display all products
+  window.onload = () => {
+    filterProduct("all");
+  };
+
+  
